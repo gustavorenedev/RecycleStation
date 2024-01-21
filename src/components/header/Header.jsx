73c5f80cf05f0705ad823c7c1f37644c3897f03d
Renan Lazoti <renanlazoti@gmail.com>
2024-01-21 16:36:17 -0300

    feature: adding header content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,14 @@ const Header = () => {
     return (  
         <ElementHeader>
             <HeaderContainer>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Scoreboard</li>
+                        <li>About</li>
+                    </ul>
+                    <button>Login</button>
+                </nav>
             </HeaderContainer>
         </ElementHeader>
     );
